cf200b9d5c20186f70554c86d738cf58eaddc004
De65 <kyalodiana6@gmail.com>
2023-06-22 15:47:12 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,20 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+# from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name= db.column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods=db.relationship('BakedGood', backref='backery')
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String)
+    prices= db.Column(db.Integer)
+    Bakery_id=db.column(db.Integer, db.ForeignKey('bakery.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
     
